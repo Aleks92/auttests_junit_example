b03470bd08ffde6cb695e721abb2858b6057cf86
Aleksandr Ivanov <aleks92ivan0v@gmail.com>
2019-04-02 21:13:21 +0300

    Push junit parametrized test example

---
+++ src/test/java/CalculatorSumTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CalculatorSumTest {
+
+    @Parameterized.Parameter
+    public int a;
+    @Parameterized.Parameter(1)
+    public int b;
+    @Parameterized.Parameter(2)
+    public int expected;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(
+                new Object[]{5, -5, 0},
+                new Object[]{1, 1, 2}
+        );
+    }
+
+    @Test
+    public void calculatorSumShouldSumTwoNumbers() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.sum(a, b);
+        assertEquals(expected, actual);
+    }
+
+}
